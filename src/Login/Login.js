cd24afe54590512ed5ed9379e5177678a16d07f1
matheusgalote <mateugalote@gmail.com>
2022-07-24 15:27:24 -0300

    Estilizando login page

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components';
+import Footer from '../Geral/Footer';
 import background from '../img/anime.jpg';
 import logo from '../img/logo.svg';
 import Button from './Button';
@@ -9,13 +10,14 @@ const Container = styled.div`
   display: grid;
   grid-template-columns: minmax(30rem, 38rem) 1fr;
   height: 100vh;
-  background: #2C3639;
+  background: #FEFBF6;
 `;
 
 const Content1 = styled.div`
   background: ${({ color }) => color };
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 5% 30% 20% 20% 20% 1%;
   justify-content: center;
   align-items: center;
 `;
@@ -35,12 +37,16 @@ const Image = styled.img`
 `;
 
 const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
   margin-top: 2rem;
 `;
 
 const Logo = styled.img`
   display: block;
+  margin: 0 auto;
   width: 5rem;
 
 `;
@@ -54,6 +60,7 @@ const Login = () => {
   return (
     <Container>
       <Content1>
+        <h1 style={{fontFamily: 'IBM Plex Sans', textAlign: 'center', margin: '1rem 0 0 0'}}>My Anime List</h1>
         <Logo src={logo} />
         <Form>
           <Input 
@@ -74,12 +81,12 @@ const Login = () => {
           />
           <Button title="Entrar" />
         </Form>
+        <Footer/>
       </Content1>
       <Content color='#ccc'>
         <Image src={background}/>        
       </Content>
     </Container>
-
   )
 }
 
